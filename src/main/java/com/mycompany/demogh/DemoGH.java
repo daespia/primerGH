7faf50435df3b6d94ff7e5e35139310f6ffe1b01
despinosa <despinosa@iescomercio.com>
2025-02-19 16:18:00 +0100

    ad demogh

---
+++ src/main/java/com/mycompany/demogh/DemoGH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demogh;
+
+/**
+ *
+ * @author ProfVespertino
+ */
+public class DemoGH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
